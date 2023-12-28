f76f5c037d1d3339370af6703f4bc3b4debd19af
viveksrisbs <98542821+viveksrisbs@users.noreply.github.com>
2023-12-28 18:30:10 +0000

    variable initialization

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
 console.log("Jai Shree Ganesh")
+const accountId = 144553
+let accountEmail="hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity])
